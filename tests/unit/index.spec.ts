189adda496702563aaef1aeb43be87bc63cea709
ga-devfront <alexis.guyomar@prestashop.com>
2023-07-04 14:10:24 +0200

    feat: add tests

--- tests/unit/index.spec.ts
+++ tests/unit/index.spec.ts
@@ -1,9 +1,36 @@
+import BSCompatibilityLayer from '../../src/js/index';
+
 describe('test for test', () => {
-  test('if BSCompatibilityLayer init', () => {
-    const consoleSpy = jest.spyOn(console, 'log');
-    require('../../src/js/index');
-    expect(consoleSpy).toHaveBeenCalledWith('test');
-    consoleSpy.mockRestore();
+  test('test data attributes updated correctly', () => {
+    const el = document.createElement('div');
+    el.setAttribute('data-autohide', 'true');
+    el.setAttribute('data-content', 'content');
+    el.setAttribute('data-toggle', 'popover');
+    document.body.appendChild(el);
+    BSCompatibilityLayer.updateDataAttributes();
+    expect(el.getAttribute('data-autohide')).toBe('true');
+    expect(el.getAttribute('data-content')).toBe('content');
+    expect(el.getAttribute('data-toggle')).toBe('popover');
+    expect(el.getAttribute('data-bs-autohide')).toBe('true');
+    expect(el.getAttribute('data-bs-content')).toBe('content');
+    expect(el.getAttribute('data-bs-toggle')).toBe('popover');
+  });
+
+  test('test data attributes not updated if not in data to update object', () => {
+    const el = document.createElement('div');
+    el.setAttribute('data-foo', 'bar');
+    BSCompatibilityLayer.updateDataAttributes();
+    expect(el.hasAttribute('data-foo')).toBe(true);
+    expect(el.hasAttribute('data-bs-foo')).toBe(false);
+  });
+
+  test('test data attributes not updated if value is undefined', () => {
+    const el = document.createElement('div');
+    el.setAttribute('data-autohide', '');
+    document.body.appendChild(el);
+    BSCompatibilityLayer.updateDataAttributes();
+    expect(el.hasAttribute('data-autohide')).toBe(true);
+    expect(el.hasAttribute('data-bs-autohide')).toBe(false);
   });
 });
 
